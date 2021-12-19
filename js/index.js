652b9d49dd3b384d180a009fb26c434d7de96115
Vasily A <newxjob@gmail.com>
2021-12-19 19:58:07 +0300

    Append support bind and unbid keyboard via button

--- js/index.js
+++ js/index.js
@@ -5,16 +5,38 @@ import { DrawUnit } from "./draw-unit.js";
 import { KeyBinder } from "./key-binder.js";
 
 const root = document.getElementById("app");
+const bindKB = document.getElementById("bind-kb");
+
 if (!root) {
   throw new Error("Not found root element");
 }
 
+if (!bindKB) {
+  throw new Error("Not found bind keyboard button element");
+}
+
 const drawUnit = new DrawUnit({ root, width: 800, height: 600 });
 const executor = new CommandExecutor(drawUnit);
-const keyBinder = new KeyBinder(executor, drawUnit);
-keyBinder.enable();
-
 drawUnit.reset();
+
+const keyBinder = new KeyBinder(executor, drawUnit);
+const BIND_KB_TEXT = "Bind keyboard";
+const UNBIND_KB_TEXT = "Unbind keyboard";
+
+bindKB.innerText = BIND_KB_TEXT;
+let isKeyBind = false;
+
+bindKB.addEventListener("click", () => {
+  isKeyBind = !isKeyBind;
+  if (isKeyBind) {
+    bindKB.innerText = UNBIND_KB_TEXT;
+    keyBinder.enable();
+  } else {
+    bindKB.innerText = BIND_KB_TEXT;
+    keyBinder.disable();
+  }
+});
+
 
 window.EX = executor;
 window.APP = drawUnit;
